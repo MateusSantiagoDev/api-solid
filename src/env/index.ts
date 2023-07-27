8ff4de2a4a6204d76c9387786c4e8d566193ebb7
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-07-27 10:48:36 -0300

    feat: eslint and prettier configuration created01~

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,22 +1,22 @@
 // carregar e validar variáveis de ambiente
 
-import "dotenv/config";
-import { z } from "zod";
+import 'dotenv/config'
+import { z } from 'zod'
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
-});
+})
 
 // validando se o process.env tem as informações
 // iguais a configuração envSchema
-const _env = envSchema.safeParse(process.env);
+const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-  console.error("❌ Invalid environment variables", _env.error.format());
+  console.error('❌ Invalid environment variables', _env.error.format())
 
   // em caso de erro o thow vai derrubar a aplicação
-  throw new Error("Invalid environment variables")
+  throw new Error('Invalid environment variables')
 }
 
 export const env = _env.data
