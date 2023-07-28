db8d18057ae9d643f434e9841a70d412e59b7df0
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-07-27 21:45:42 -0300

    feat: add create user route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,28 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
+app.post('/users', async (request, reply) => {
+  // validação no email e no minimo 6 caracteres na senha
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
 
+  // validação usando o parse
+  const { name, email, password } = registerBodySchema.parse(request.body)
 
+  // criação dos dados
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
+})
